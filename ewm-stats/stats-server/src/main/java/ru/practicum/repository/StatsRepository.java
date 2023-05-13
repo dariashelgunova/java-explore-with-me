5e528555e3349fee3a49d2746b0e05161d575456
dariashelgunova <dariashelgunova@yandex.ru>
2023-05-13 12:13:07 +0300

    Commit 4: corrected mistakes

--- ewm-stats/stats-server/src/main/java/ru/practicum/repository/StatsRepository.java
+++ ewm-stats/stats-server/src/main/java/ru/practicum/repository/StatsRepository.java
@@ -2,14 +2,12 @@ package ru.practicum.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 import ru.practicum.model.Hit;
 import ru.practicum.view.HitView;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Repository
 public interface StatsRepository extends JpaRepository<Hit, Integer> {
 
     @Query(value = "select h.app, h.uri, count(DISTINCT h.ip) as hitsAmount " +
